b8694dd1c66ca9aa5af0c024748e642ec1a4de68
Bharat <hibharatj@gmail.com>
2025-07-04 12:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let submitbtn = document.getElementById('btn')
+let output = document.getElementById('output')
+
+submitbtn.addEventListener('click', async () => {
+
+    let displayText = document.getElementById('text').value.trim();
+    let delay = Number(document.getElementById('delay').value.trim()) 
+
+    if(displayText == "" || delay == "") {
+        alert('enter both values')
+        return ;
+    }
+    console.log(displayText, delay)
+
+    document.getElementById('text').value = '';
+    document.getElementById('delay').value = ''; 
+
+    output.innerText = 'waiting...'
+
+    await callingfunc(delay*1000);
+    output.innerText = displayText;
+
+})
+
+function callingfunc(ms) {
+    return new Promise (function (resolve) {
+        setTimeout(() => {
+            resolve();
+        }, ms);
+    })
+}
